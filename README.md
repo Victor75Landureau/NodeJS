4e46514cc8d65a408d2861bd9d4af2bb246bc447
Victor75Landureau <victor.landureau@gmail.com>
2017-02-08 02:35:04 +0100

    Final commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # NodeJS
-NodeJS website 
+NodeJS LoginAPP
+
+npm init
+npm update
+node app.js
+=> localhost 3000
--- routes/index.js
+++ routes/index.js
@@ -1,50 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-var Video = require('../models/video');
 var User = require('../models/user');
 
 // Get Homepage
 // router.get('/', ensureAuthenticated, function(req, res){
 router.get('/', function(req, res){
 	res.render('index');
-});
-
-// Get Video page
-router.get('/video', function(req, res){
-	res.render('video');
-});
-
-//Add Video
-router.post('/video', function(req, res, username){
-	// var video_user = User.username;
-	var video_title = req.body.video_title;
-	var video_link = req.body.video_link;
-
-	//Validation
-	req.checkBody('video_title','Title is required').notEmpty();
-	req.checkBody('video_link','Link is required').notEmpty();
-
-	var errors = req.validationErrors();
-	if(errors){
-		console.log('Errors:', {errors});
-		res.render('video');
-	}else {
-		var newVideo = new Video({
-			// username: User.getUserByUsername(),
-			video_title:video_title,
-			video_link:video_link
-		});
-
-		Video.createVideo(newVideo, function(err, user){
-			if(err) throw err;
-			console.log(user);
-		});
-
-		req.flash('success_msg', 'Your video has been loaded');
-
-		res.redirect('video');
-	}
 });
 
 // Get Contact page
